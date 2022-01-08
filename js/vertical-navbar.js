8cec2361eabf765bda04200c37b3eaf98d78adc6
assane <assane.diouf@centrale-marseille.com>
2022-01-08 21:40:31 +0100

    C'est bon c'est fait ! (même si l'animation semble un peu étrange)

--- js/vertical-navbar.js
+++ js/vertical-navbar.js
@@ -4,11 +4,13 @@ let toggler = document.getElementById("my-toggler");
 
 function update() {
     if(!navbar.classList.contains("show")) {
-        body.classList.remove("body-shift")
+        body.classList.remove("body-shift");
+        toggler.hidden = false;
     } else {
-        body.classList.add("body-shift")
+        body.classList.add("body-shift");
+        toggler.hidden = true;
     }
-    setTimeout(update, 500);
+    setTimeout(update, 10);
 }
 
 update();
